bc27ebf1adbd2364ff74d48d963377c064d650fd
Sohag Abir <sohagabir112@gmail.com>
2025-11-23 23:23:32 +0600

    Add files via upload

---
+++ Basic Concept/GreaterNumber.cpp
@@ -0,0 +1,17 @@
+//Greatest of two Numbers
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+    if(a>b)
+    {
+        cout<<"a is the greatest number"<<endl;
+    }
+    else{
+        cout<<"b is the greatest number"<<endl;
+    }
+
+    return 0;
+}
