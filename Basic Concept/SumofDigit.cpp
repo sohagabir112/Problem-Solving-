1b46f6221840e0ac983c280c0da42b162bc05c6e
Sohag Abir <sohagabir112@gmail.com>
2025-11-27 23:23:49 +0600

    Add files via upload

---
+++ Basic Concept/SumofDigit.cpp
@@ -0,0 +1,15 @@
+// Sum of digits of a number
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, sum = 0;
+    cin>>n;
+    while(n){
+        sum += n%10;
+        n/=10;
+    }
+    cout<<"Sum of digits: "<<sum<<endl;
+
+    return 0;
+}
