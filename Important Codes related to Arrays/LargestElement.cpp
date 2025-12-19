0c2d293ee05bc402b805d55498ce1307a7738692
Sohag Abir <sohagabir112@gmail.com>
2025-12-19 21:59:59 +0600

    Add files via upload

---
+++ Important Codes related to Arrays/LargestElement.cpp
@@ -0,0 +1,26 @@
+//Find Largest element in an array 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Array Size: "<<endl;
+    cin>>n;
+    int arr[n];
+    int max = arr[0];
+
+    for(int i = 0; i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i= 1;i<n;i++)
+    {
+        if(max<arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    cout<<max;
+
+    return 0;
+}
