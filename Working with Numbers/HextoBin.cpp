8e1ca6c0c7b8fc3956c546bd6a22fb73c984daa4
Sohag Abir <sohagabir112@gmail.com>
2026-02-23 23:50:01 +0600

    Hexadecimal to Binary conversion

---
+++ Working with Numbers/HextoBin.cpp
@@ -0,0 +1,19 @@
+//Hexadecimal to binary conversion
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+void convertBinary(string hex){
+    long long bin =0;
+    int i=0,count=1,pos=0;
+    
+     
+}
+
+int main(){
+    string hex;
+    cin>>hex;
+    convertBinary(hex);
+
+    return 0;
+}
